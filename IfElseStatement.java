d15f3fad055016f40107fb35c7768d3ca832048b
praveen <praveenreddyvalle@gmail.com>
2023-06-22 10:15:11 -0400

    IfElse example

---
+++ IfElseStatement.java
@@ -0,0 +1,18 @@
+public class IfElseStatement {
+   public static void main(String []args) {
+   
+   int age=20;
+
+if(age>=18){
+System.out.println("Person is eligible to vote");
+}
+else{
+System.out.println("person is not eligible to vote");
+}
+
+   
+   
+      
+   
+   }
+   }
